50047047a1fd2c12758ce3847ea4695cc083ba53
suraj <suraz63>
2022-05-25 17:58:02 +0545

    feat: convert kilograms to lbs

--- src/UnitConversionClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace UnitConversion\UnitConversion;
-
-class UnitConversionClass
-{
-}
---
+++ src/Weight.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Suraj\UnitConversions;
+
+class Weight
+{
+    private float $kilograms;
+
+    public function __construct(float $kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
+
+    public static function fromKilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+
+}
